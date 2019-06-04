31ae96a257ed0b15d1fb28663cb16309f5447680
Kiran BG <kodeutility@gmail.com>
2019-06-04 11:43:42 +0530

    Coding Problems folder added
    The folder contains coding solutions for various problems

---
+++ Coding problems/arrays/01_program_for_array_rotation.py
@@ -0,0 +1,12 @@
+#funcation rotate(arr[],d,n) that rotates arr[] of size n by d elements.
+# E.g [1,2,3,4,5] After 2 rotations [4,5,1,2,3]
+
+def rotate(arr,d,n):
+    no_of_rotations = d%n
+    part_1 = arr[:no_of_rotations]
+    part_2 = arr[no_of_rotations:]
+    return part_2 + part_1
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+print(rotate(arr,15,10))
